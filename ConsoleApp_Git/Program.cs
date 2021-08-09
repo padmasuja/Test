50d631eb859b63e97034efb5e7bb2173020006c9
Nair <PNair@clinipace.com>
2021-08-09 23:48:55 +0530

    Add project files.

---
+++ ConsoleApp_Git/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp_Git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
